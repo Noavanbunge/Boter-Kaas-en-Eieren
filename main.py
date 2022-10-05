01df1c894e7465cf5c7ce0e3fbecd83689f0401b
Noa van <78596993+Noavanbunge@users.noreply.github.com>
2022-10-05 07:56:25 +0000

    gekoppeld github

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from bke import EvaluationAgent, start
 
 
 class MyRandomAgent(EvaluationAgent):
-  def evaluate(self, board, my_symbol, opponent_symbol:
+  def evaluate(self, board, my_symbol, opponent_symbol):
       return random.randint(1, 500)
 
 
 my_random_agent = MyRandomAgent()
-start(player_o=my_random_agent)     
+start(player_o=my_random_agent)    
+ 
